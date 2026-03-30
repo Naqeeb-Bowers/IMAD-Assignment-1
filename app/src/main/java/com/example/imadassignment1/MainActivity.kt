4dc8f775c49a63b3c1e26b15880e61550942c575
Naqeeb-Bowers <naqeebbowers18@gmail.com>
2026-03-30 16:29:20 +0200

    first commit

---
+++ app/src/main/java/com/example/imadassignment1/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.imadassignment1
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super .onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.startButton)
+
+        startButton.setOnClickListener { it: View? ->
+            val intent = intent(this, mainActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
