4d33167ace5df5099a4697b6a9db2ed6861a8e49
Naqeeb-Bowers <naqeebbowers18@gmail.com>
2026-03-30 20:38:59 +0200

    both screens done
    coding completed without aesthetics

--- mainscreenass-1/src/main/java/com/example/mainscreenass_1/mainScreen.kt
+++ mainscreenass-1/src/main/java/com/example/mainscreenass_1/mainScreen.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity (mainScreen) {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
