507ab9befbd9b77e34724a24709227e17159d9e0
Naqeeb-Bowers <naqeebbowers18@gmail.com>
2026-03-30 21:58:45 +0200

    suppressed minor errors
    removed unused code and suppressed errors

--- app/src/main/java/com/example/imadassignment1/MainActivity.kt
+++ app/src/main/java/com/example/imadassignment1/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.imadassignment1
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
