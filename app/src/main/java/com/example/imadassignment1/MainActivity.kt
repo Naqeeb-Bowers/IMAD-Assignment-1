5da4d6aa5ed125650c7d2803b01a3d6772ed6339
Naqeeb-Bowers <naqeebbowers18@gmail.com>
2026-03-30 20:10:40 +0200

    changes to mainActivity.kt

--- app/src/main/java/com/example/imadassignment1/MainActivity.kt
+++ app/src/main/java/com/example/imadassignment1/MainActivity.kt
@@ -7,19 +7,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import android.widget.Button
 
-private val MainActivity.mainActivity
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState : Bundle?) {
+
         super .onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val startButton = findViewById<Button>(R.id.startButton)
 
-        startButton.setOnClickListener { it: View? ->
-            val intent = intent(this, mainActivity:)
-            startActivity(intent)
+        startButton.setOnClickListener {
 
         }
     }
